b334e3d8fda3ac5e0bfe1bf0824e08e0dba1ddb1
PriBoris <prishchepenko.boris@gmail.com>
2016-09-20 11:22:10 +0300

    component placement done

--- Hardware/clean.bat
+++ Hardware/clean.bat
@@ -4,6 +4,7 @@ del History\*.* /s /q
 del *.log /s /q
 
 rd "Project Logs for MBLv1" /q /s
+rem rd "Project Outputs for MBLv1" /q /s
 rd "History" /q /s
 rd "pcb/__Previews" /q /s
 rd "sch/__Previews" /q /s
