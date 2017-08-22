742d397e229a35d2188a3d0a20b3461681255976
PriBoris <prishchepenko.boris@gmail.com>
2017-08-22 08:54:10 +0300

    Init and setup Keil project

---
+++ Firmware/RTE/RTE_Components.h
@@ -0,0 +1,16 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'MBLv1' 
+ * Target:  'MBLv1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_USB_Core                    /* USB Core */
+#define RTE_USB_Device_HID_0            /* USB Device HID instance 0 */
+
+#endif /* RTE_COMPONENTS_H */
---
+++ Firmware/src/main.cpp
@@ -0,0 +1,16 @@
+
+
+#include <stdint.h>
+
+
+int main(){
+	
+	uint32_t i = 0;
+	while(true){ 
+		i++;
+		__asm("  nop");
+		
+	};
+	
+}
+ 
